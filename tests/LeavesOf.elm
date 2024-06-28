03805f627dd4ae7c3a16cd33894339b3bf07ce8a
hisusqristos <varyadaser@gmail.com>
2024-06-28 15:40:28 +0400

    :golf: returns leaves խշխշխշ

--- tests/LeavesOf.elm
+++ tests/LeavesOf.elm
@@ -1,18 +1,69 @@
 module LeavesOf exposing (..)
 
 import Expect
+import Main exposing (Tree(..))
 import Test exposing (..)
 
 
 suite : Test
 suite =
-    skip <| describe "calculate leaves of a tree"
-        [ test """ 
+    describe "calculate leaves of a tree"
+        [ test "single noded tree is its own leaf" <|
+            \() ->
+                let
+                    tree =
+                        Tree
+                            { self = 20
+                            , parent = 0
+                            , change = 30
+                            , children = Nothing
+                            }
+
+                    leaves : List Tree
+                    leaves =
+                        [ tree
+                        ]
+
+                    result =
+                        leavesOf tree
+                in
+                Expect.equal result leaves
+        , test """ 
               a   tree with one node
               |   has an only leaf (c)
               c
             """ <|
-            \() -> Expect.fail  "unimplemented"
+            \() ->
+                let
+                    tree =
+                        Tree
+                            { self = 20
+                            , parent = 0
+                            , change = 30
+                            , children =
+                                Just
+                                    [ Tree
+                                        { self = 20
+                                        , parent = 20
+                                        , change = 10
+                                        , children = Nothing
+                                        }
+                                    ]
+                            }
+
+                    leaves =
+                        [ Tree
+                            { self = 20
+                            , parent = 20
+                            , change = 10
+                            , children = Nothing
+                            }
+                        ]
+
+                    result =
+                        leavesOf tree
+                in
+                Expect.equal result leaves
         , test """
             a
           __|__
@@ -20,5 +71,63 @@ suite =
         __|__
         d   e
         """ <|
-            \() -> Expect.fail  "unimplemented"
+            \() ->
+                let
+                    tree =
+                        Tree
+                            { self = 20
+                            , parent = 0
+                            , change = 30
+                            , children =
+                                Just
+                                    [ leaf1
+                                    , Tree
+                                        { self = 20
+                                        , parent = 20
+                                        , change = 10
+                                        , children = Just leaves2
+                                        }
+                                    ]
+                            }
+
+                    leaf1 =
+                        Tree
+                            { self = 20
+                            , parent = 20
+                            , change = 10
+                            , children = Nothing
+                            }
+
+                    leaves2 =
+                        [ Tree
+                            { self = 80
+                            , parent = 40
+                            , change = 20
+                            , children = Nothing
+                            }
+                        , Tree
+                            { self = 30
+                            , parent = 20
+                            , change = 40
+                            , children = Nothing
+                            }
+                        ]
+
+                    leaves =
+                        leaf1 :: leaves2
+
+                    result =
+                        leavesOf tree
+                in
+                Expect.equal result leaves
         ]
+
+
+leavesOf : Tree -> List Tree
+leavesOf ((Tree { children }) as tree) =
+    case children of
+        Nothing ->
+            [ tree ]
+
+        Just childNodes ->
+            List.concatMap leavesOf childNodes
